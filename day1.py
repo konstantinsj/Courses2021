79890731bdcc0ebda51b6abcd9781701f94f5678
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-09-20 18:11:18 +0300

    day1 hw

---
+++ day1.py
@@ -0,0 +1,18 @@
+#
+# TODO Write the following code in Python:
+#
+
+# print your name 5 times
+print("Konstantins \n" *5)
+
+# print Bananananana using "Ba" once and "na" 5 times
+print("Ba" + "na" * 5)
+
+# print the number of seconds in a year calculated from months, days, minutes, seconds
+print(60 * 60 * 24 * 365)
+
+# how much a googol is that is 10 to the power of the 100th
+print(10**100)
+
+# how much will your 1000 Euro investment be worth after 12 years of 6% yearly interest?
+print(1_000 * 1.06**12) 
