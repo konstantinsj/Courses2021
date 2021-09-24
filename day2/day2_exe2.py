73b92908dfc06b9da54abfeefe671f8cbfc3ace3
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-09-24 17:57:30 +0300

    tidy up

--- day2/day2_exe2.py
+++ day2/day2_exe2.py
@@ -1,11 +1,16 @@
 # 2. Exercise - Room volume
 
 # Ask user to input 3 numbers - width, length, height
-# Find the volume of the room
-# PS Think about units and what is the most appropriate data type for this
 
 width = float(input("Please input width "))
 length = float(input("Please input length "))
 height = float(input("Please input height "))
+
+# Find the volume of the room
+
 volume = width*length*height
+
+# PS Think about units and what is the most appropriate data type for this
 print(f"Volume of the room is {round(volume,2)}")
+
+
