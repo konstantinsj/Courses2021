181b37bf0bcc0e2bc6b49765cd8dfb8f9020f40f
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-09-27 17:39:57 +0300

    optimize

--- day3/day3_1.py
+++ day3/day3_1.py
@@ -9,9 +9,9 @@ if user_temperature < 35:
     print("Not too cold")
 
 # If 35 to 37 (inclusive), output "all right"
-if 35 <= user_temperature <= 37:
+elif user_temperature <= 37:
     print("Alright")
 
 # If the temperature  over 37, then output  "possible fever"
-if 37 < user_temperature:
+else:
     print("possible fever!")
