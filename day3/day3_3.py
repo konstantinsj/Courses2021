76c6a0b81fbb189e5c8a7bbaf0a0f107db61b489
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-09-27 21:49:19 +0300

    refactor

--- day3/day3_3.py
+++ day3/day3_3.py
@@ -2,30 +2,28 @@
 
 # Ask the user to enter 3 numbers, output them in ascending order.
 
-num1 = float(input("Type your number 1: "))
-num2 = float(input("Type your number 2: "))
-num3 = float(input("Type your number 3: "))
+num1 = -9
+num2 = 15
+num3 = 0
 
 # output them in ascending order.
-#TODO this is ugly :)
-if num1 < num2 and num1 < num3:
-    if num2 < num3:
-        print(num1,num2,num3)
-    else:
-        print(num1,num3,num2)
-elif num2 < num3 and num2 < num3:
-    if num3 < num1:
-        print(num2,num3,num1)
-    else:
-        print(num2,num1,num3)
-elif num3 < num1 and num3 < num2:
-    if num2 < num1:
-        print(num3,num2,num1)
-    else:
-        print(num3,num1,num2)
 
+if num1 < num2:
+    tmp = 0
+    tmp = num1
+    num1 = num2
+    num2 = tmp
+if num1 < num3:
+    tmp = 0
+    tmp = num1
+    num1 = num3
+    num3 = tmp
+if num2 < num3:
+    tmp = 0
+    tmp = num2
+    num2 = num3
+    num3 = tmp
+print(num3, num2, num1)
 
 # Note: for now, we solve this task only with if, elif, else actions.
-
 # There is also a solution using sorting and list structure, which we have not yet seen.
-
