6cb42b830f2b62a493ea4d668884301303093357
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-10-25 17:10:03 +0300

    day11

--- day11/day11.py
+++ day11/day11.py
@@ -23,3 +23,40 @@ def get_shuffled_cards():
 get_shuffled_cards()
 
 
+# 2. Deck
+#
+# write a class Deck with the following attributes(variables)
+# available - contains list of card tuples that can be used
+# spent - contains list of card tuples that have been used
+# there should be following methods:
+
+# constructor (__init__) method
+# initializes available with full 52 card list of tuples
+# initializes spent with empty list
+# shuffle(self):
+#
+# # shuffles available cards - works just like 1st function but works on available
+# get_cards(self, count=1):
+# # gets some number(default 1) of cards from available
+# adds these cards to spent
+# returns these cards
+#
+# # you can add other methods and/or attributes if you wish to Deck class
+
+
+class Deck:
+
+    def __init__(self, card_list=""):
+        suit = "diamonds ♦", "hearts ♥", "spades ♠", "clubs ♣"
+        card = 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"
+        self.card_list = list(itertools.product(suit, card))
+        print(self.card_list)
+
+    def shuffle(self):
+        random_card = random.choice(self.card_list)
+        print(random_card)
+        return self
+
+
+new_deck = Deck()
+random_card = Deck.shuffle(new_deck)
