2e0f760039e8ad31e17ed8ec2ac74b5c96fb0f54
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-10-25 17:27:36 +0300

    WIP

--- day11/day11.py
+++ day11/day11.py
@@ -12,15 +12,15 @@ import itertools
 import random
 
 
-# def get_shuffled_cards():
-#     suit = "diamonds ♦", "hearts ♥", "spades ♠", "clubs ♣"
-#     card = 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"
-#     card_list = list(itertools.product(suit, card))
-#     random_card = random.choice(card_list)
-#     print(random_card)
-#
-#
-# get_shuffled_cards()
+def get_shuffled_cards():
+    suit = "diamonds ♦", "hearts ♥", "spades ♠", "clubs ♣"
+    card = 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"
+    card_list = list(itertools.product(suit, card))
+    random_card = random.choice(card_list)
+    print(random_card)
+
+
+get_shuffled_cards()
 
 
 # 2. Deck
