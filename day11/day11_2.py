69d982c69bf0131ffdcac47224ee8520abc9ac89
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-10-25 23:20:07 +0300

    day11

--- day11/day11_2.py
+++ day11/day11_2.py
@@ -24,37 +24,33 @@ import random
 #
 # # you can add other methods and/or attributes if you wish to Deck class
 
-
 class Deck:
 
     def __init__(self, card_list="", spent=""):
         suit = "diamonds ♦", "hearts ♥", "spades ♠", "clubs ♣"
-        card = "K", "A"
-        # suit = "diamonds ♦", "hearts ♥", "spades ♠", "clubs ♣"
-        # card = 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"
+        value = 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"
         self.spent = spent
-        self.card_list = (list(itertools.product(suit, card)))
-
-    def display_cards(self):
-        print(f"Available cards {self.card_list}")
+        self.card_list = (list(itertools.product(suit, value)))
 
     def shuffle(self):  # shuffle cards
         random.shuffle(self.card_list)
-        print(f"Shuffled cards: {self.card_list}")
         return self
 
     def get_cards(self, count=1):
         if count > len(self.card_list):
-            print("Cant get that much!")
+            print("Cant get that much!")  # checking if trying to get more cards than available
         else:
             self.spent = self.card_list[:count]
-            del self.card_list[0:count]
-            print(f"Got cards: {self.spent}")
-            # self.card_list = list(set(self.card_list)-set(self.spent))  set solution, but cards are shuffled
-        return self
+            del self.card_list[0:count]  # fastest solution
+
+            # for card in self.spent:           # this is not a fast
+            #    self.card_list.remove(card)
+            # self.card_list = list(set(self.card_list)-set(self.spent)) # set solution, but cards are shuffled
+        return self.spent
 
 
 new_deck = Deck()
 new_deck.shuffle()
-new_deck.get_cards()
-new_deck.display_cards()
+print(new_deck.card_list)
+print(new_deck.get_cards())
+print(new_deck.card_list)
