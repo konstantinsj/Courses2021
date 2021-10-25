e20e74e1f851cbba027a663f0476cb35e25e32f8
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-10-25 17:26:55 +0300

    WIP

--- day11/day11.py
+++ day11/day11.py
@@ -12,15 +12,15 @@ import itertools
 import random
 
 
-def get_shuffled_cards():
-    suit = "diamonds ♦", "hearts ♥", "spades ♠", "clubs ♣"
-    card = 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"
-    card_list = list(itertools.product(suit, card))
-    random_card = random.choice(card_list)
-    print(random_card)
-
-
-get_shuffled_cards()
+# def get_shuffled_cards():
+#     suit = "diamonds ♦", "hearts ♥", "spades ♠", "clubs ♣"
+#     card = 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"
+#     card_list = list(itertools.product(suit, card))
+#     random_card = random.choice(card_list)
+#     print(random_card)
+#
+#
+# get_shuffled_cards()
 
 
 # 2. Deck
@@ -33,9 +33,10 @@ get_shuffled_cards()
 # constructor (__init__) method
 # initializes available with full 52 card list of tuples
 # initializes spent with empty list
+
 # shuffle(self):
-#
 # # shuffles available cards - works just like 1st function but works on available
+
 # get_cards(self, count=1):
 # # gets some number(default 1) of cards from available
 # adds these cards to spent
@@ -46,13 +47,15 @@ get_shuffled_cards()
 
 class Deck:
 
-    def __init__(self, card_list=""):
+    def __init__(self, card_list="", spent=""):
         suit = "diamonds ♦", "hearts ♥", "spades ♠", "clubs ♣"
         card = 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"
-        self.card_list = list(itertools.product(suit, card))
+        self.card_list = (list(itertools.product(suit, card)))
+        self.spent = ""
+        available = set(card_list) - set(spent)
         print(self.card_list)
 
-    def shuffle(self):
+    def shuffle(self):  #random card
         random_card = random.choice(self.card_list)
         print(random_card)
         return self
