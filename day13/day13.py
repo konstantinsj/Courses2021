f683037414d4fbe807e7620c190433ab090b4a7d
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-11-01 17:23:00 +0200

    refactor

--- day13/day13.py
+++ day13/day13.py
@@ -6,7 +6,7 @@ import requests
 def get_crypto_prices():
     url = "https://api2.binance.com/api/v3/ticker/24hr"
     response = requests.get(url)  # so we made a HTTP GET request here just like a browser would
-    ticker = dict()                # dict with trading pair prices
+    prices = dict()                # dict with trading pair prices
 
     print(response.status_code)  # Response Code 200 is good! 404 not good :)
     data_from_json = response.json()
@@ -14,11 +14,11 @@ def get_crypto_prices():
     for i in range(len(data_from_json)):    # we getting only last prices
         symbol = data_from_json[i].get("symbol")
         last_price = data_from_json[i].get("lastPrice")
-        ticker[i] = symbol, last_price
+        prices[i] = symbol, last_price
 
     with open("data_from_url.json", mode="w") as write_file:    # saving data to json
-        json.dump(ticker, write_file, indent=4)
+        json.dump(prices, write_file, indent=4)
 
-    return ticker
+    return prices
 
 print(get_crypto_prices())
