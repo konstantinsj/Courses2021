9e5da75263614deac55b4ca8a913081a2e201824
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-11-05 22:17:41 +0200

    refactor

--- work/main.py
+++ work/main.py
@@ -10,17 +10,22 @@ start_time = time.time()
 browser = webdriver.Chrome('chromedriver')  # path to chromedriver
 
 browser.get('https://inch.lv/browse?type=apartment&districts=R%C4%ABga&subdistricts=Centrs%2CVecr%C4%ABga&dealType'
-            '=sale&page=1');  #
+            '=sale');  #
+next_page = "browse__pagination__next"
+scroll_into_view = "arguments[0].scrollIntoView();"
+card_list = 'browse-card-list__data'
+cards = "browse-card-wrapper"
 
 time.sleep(1)
-elements = browser.find_element(By.CLASS_NAME, 'browse-card-list__data')
-ads = elements.find_elements(By.CLASS_NAME, "browse-card-wrapper")
+elements = browser.find_element(By.CLASS_NAME, card_list)
+ads = elements.find_elements(By.CLASS_NAME, cards)
 price = list()
 for i in range(len(ads)):   #iterating elements on page
     price.append(ads[i].find_element(By.CLASS_NAME, "browse-card__cost__price").text)
-    browser.execute_script("arguments[0].scrollIntoView();", ads[i])
-if browser.find_element(By.CLASS_NAME, "browse__pagination__next").is_displayed():
-    browser.find_element(By.CLASS_NAME, "browse__pagination__next").click()
+    browser.execute_script(scroll_into_view, ads[i])
+if browser.find_element(By.CLASS_NAME, next_page).is_displayed():
+    browser.find_element(By.CLASS_NAME, next_page).click()
+
 print(price)
 print("--- %s seconds ---" % (time.time() - start_time))
 
