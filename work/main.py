c9a9b0645e10969492bb73216865bce6bd29eafd
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-11-05 23:52:35 +0200

    multiple pages

--- work/main.py
+++ work/main.py
@@ -3,29 +3,40 @@ import time
 
 import json
 import requests
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 start_time = time.time()
 browser = webdriver.Chrome('chromedriver')  # path to chromedriver
-
-browser.get('https://inch.lv/browse?type=apartment&districts=R%C4%ABga&subdistricts=Centrs%2CVecr%C4%ABga&dealType'
-            '=sale');  #
 next_page = "browse__pagination__next"
 scroll_into_view = "arguments[0].scrollIntoView();"
 card_list = 'browse-card-list__data'
 cards = "browse-card-wrapper"
+next_page_disabled = "browse__pagination__border.browse__pagination__border--disabled"
 
+browser.get('https://inch.lv/browse?type=apartment&districts=R%C4%ABga&subdistricts=Centrs%2CVecr%C4%ABga&page=1&priceTo=60000&dealType=sale');  #
 time.sleep(1)
-elements = browser.find_element(By.CLASS_NAME, card_list)
-ads = elements.find_elements(By.CLASS_NAME, cards)
 price = list()
-for i in range(len(ads)):   #iterating elements on page
-    price.append(ads[i].find_element(By.CLASS_NAME, "browse-card__cost__price").text)
-    browser.execute_script(scroll_into_view, ads[i])
-if browser.find_element(By.CLASS_NAME, next_page).is_displayed():
-    browser.find_element(By.CLASS_NAME, next_page).click()
 
+while browser.find_element(By.CLASS_NAME, next_page).is_displayed():
+    time.sleep(1)
+    elements = browser.find_element(By.CLASS_NAME, card_list)
+    ads = elements.find_elements(By.CLASS_NAME, cards)
+    print(f"Got {len(ads)} results on this page")
+    for i in range(len(ads)):  # iterating elements on page
+        price.append(ads[i].find_element(By.CLASS_NAME, "browse-card__cost__price").text)
+        browser.execute_script(scroll_into_view, ads[i])
+    try:
+        browser.find_element(By.CLASS_NAME, next_page_disabled).is_displayed()
+        print("No next page!")
+        break
+    except (TimeoutException, WebDriverException) as e:
+        browser.find_element(By.CLASS_NAME, next_page).click()
+    time.sleep(3)
+
+print(f"Total results: {len(price)}")
 print(price)
 print("--- %s seconds ---" % (time.time() - start_time))
 
