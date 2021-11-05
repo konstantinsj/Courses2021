bf16653b380dc7f54da6531a505dc020e3cdaa8a
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-11-06 01:27:12 +0200

    refactor

--- work/main.py
+++ work/main.py
@@ -8,12 +8,13 @@ from selenium.webdriver.support import expected_conditions as EC
 start_time = time.time()
 browser = webdriver.Chrome('chromedriver')  # path to chromedriver
 wait = WebDriverWait(browser, 10)
-next_page = "browse__pagination__next"
+next_page = By.CLASS_NAME, "browse__pagination__next"
 scroll_into_view = "arguments[0].scrollIntoView();"
 
 card_list = By.CLASS_NAME, 'browse-card-list__data'
-cards = "browse-card-wrapper"
-next_page_disabled = "browse__pagination__border.browse__pagination__border--disabled"
+cards = By.CLASS_NAME, "browse-card-wrapper"
+cost_price = By.CLASS_NAME, "browse-card__cost__price"
+next_page_disabled = By.CLASS_NAME, "browse__pagination__border.browse__pagination__border--disabled"
 
 url = 'https://inch.lv/browse?type='
 type = 'apartment'
@@ -28,18 +29,18 @@ result = list()
 while True:
     wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'browse-card-list__data')))
     elements = browser.find_element(*card_list)
-    ads = elements.find_elements(By.CLASS_NAME, cards)
+    ads = elements.find_elements(*cards)
     print(f"Got {len(ads)} results on this page")
     for i in range(len(ads)):  # iterating elements on page
-        result.append(ads[i].find_element(By.CLASS_NAME, "browse-card__cost__price").text)
+        result.append(ads[i].find_element(*cost_price).text)
         browser.execute_script(scroll_into_view, ads[i])
     try:
-        browser.find_element(By.CLASS_NAME, next_page_disabled).is_displayed()
+        browser.find_element(*next_page_disabled).is_displayed()
         print("No next page!")
         break
     except (TimeoutException, WebDriverException):
         try:
-            browser.find_element(By.CLASS_NAME, next_page).click()
+            browser.find_element(*next_page).click()
         except (TimeoutException, WebDriverException):
             break
 
