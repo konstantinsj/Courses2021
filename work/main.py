eee6a741f935440da2997144bf674f2966c0e69a
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-11-06 01:59:11 +0200

    inchlv class

--- work/main.py
+++ work/main.py
@@ -6,11 +6,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 start_time = time.time()
+
 browser = webdriver.Chrome('chromedriver')  # path to chromedriver
 wait = WebDriverWait(browser, 10)
+
 next_page = By.CLASS_NAME, "browse__pagination__next"
 scroll_into_view = "arguments[0].scrollIntoView();"
-
 card_list = By.CLASS_NAME, 'browse-card-list__data'
 cards = By.CLASS_NAME, "browse-card-wrapper"
 cost_price = By.CLASS_NAME, "browse-card__cost__price"
